b21bbe159732a7842c577248f476a9a6df70d79a
Lsyh999 <1918192217@qq.com>
2026-07-23 17:37:50 +0800

    chore: stop tracking local server helpers

--- tools/connect_physgenloop_server.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$pythonScript = Join-Path $scriptDir "physgenloop_remote.py"
-
-python $pythonScript shell
